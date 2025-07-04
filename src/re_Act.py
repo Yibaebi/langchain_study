4c746ea49ee093f8bd3a6aa7e2327018f528eb4a
yibaebi <elliot.yibaebi@yahoo.com>
2025-07-04 21:18:24 +0100

    complete agentic workflow

--- src/re_Act.py
+++ src/re_Act.py
@@ -1,15 +1,35 @@
-from langchain.agents import tool
+from typing import List, Union
+from langchain.agents import tool, Tool
+from langchain.agents.format_scratchpad.log import format_log_to_str
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from langchain_anthropic import ChatAnthropic
+
+from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.prompts import PromptTemplate
 from langchain_core.tools.render import render_text_description
-from langchain import hub
 
 
 @tool
 def get_string_length(text: str):
     """Returns the length of a string."""
     return len(text.strip("\n").strip('"'))
+
+
+def find_tool_by_name(tools: List[Tool], tool_name: str):
+    """Returns a tool name to be executed"""
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+
+    return ValueError()
+
+
+def get_agent_input(intermediate_steps: list):
+    """Returns a static query with a dynamic intermediate steps"""
+    return {
+        "input": "What is the length of 'Elliot'?",
+        "agent_scratchpad": intermediate_steps,
+    }
 
 
 if __name__ == "__main__":
@@ -36,7 +56,7 @@ if __name__ == "__main__":
       Begin!
 
       Question: {input}
-      Thought:
+      Thought: {agent_scratchpad}
     """
 
     prompt = PromptTemplate.from_template(template=prompt_template).partial(
@@ -44,11 +64,38 @@ if __name__ == "__main__":
         tool_names=", ".join([t.name for t in tools]),
     )
 
-    llm = ChatAnthropic(model="claude-sonnet-4-20250514", temperature=0)
-    agent = (
-        {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+    llm = ChatAnthropic(
+        model="claude-sonnet-4-20250514",
+        temperature=0,
+        stop=["\nObservation"],
     )
 
-    res = agent.invoke({"input": "What is the length of this text 'Elliot'"})
+    intermediate_steps = []
 
-    print(res)
+    agent_inputs = {
+        "input": lambda x: x["input"],
+        "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"]),
+    }
+
+    agent = agent_inputs | prompt | llm | ReActSingleInputOutputParser()
+
+    agent_step: Union[AgentAction | AgentFinish] = agent.invoke(
+        get_agent_input(intermediate_steps)
+    )
+
+    if isinstance(agent_step, AgentAction):
+        # Get tool to call for next step
+        tool_name = agent_step.tool
+        tool_to_call = find_tool_by_name(tools, tool_name)
+        tool_input = agent_step.tool_input
+
+        # get observation and add it to scratchpad for memory sakes
+        observation = tool_to_call.func(tool_input)
+        intermediate_steps.append((agent_step, str(observation)))
+
+        # re-invoke the agent with the new observation and scratchpad
+        agent_step = agent.invoke(get_agent_input(intermediate_steps))
+
+        if isinstance(agent_step, AgentFinish):
+            final_answer = agent_step.return_values
+            print(final_answer)
